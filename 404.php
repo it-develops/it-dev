d168bda42784b1f78b3bf7435d070e46d8843a7f
Dmitry Ivanov <it-develops@yandex.ru>
2018-03-26 13:10:25 +0300

    edit

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@ get_header(); ?>
 <section class="block_404 center">
 	<div class="container">
 		<div class="wrapper404" data-animated="fadeIn">
-			<h1 class="margbot0">Ой! Страница отстутствует :(</h1>
+			<h1 class="margbot0">Ой! Страница отcутствует :(</h1>
 			<h4>Извините, но мы не смогли найти то, что вы искали...</h4>			
 		</div>
 	</div>
